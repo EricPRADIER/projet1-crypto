e0d9042f272e71cd17c2770bbae03a79244c5e13
Gregoria R <rubio.gregoria@gmail.com>
2022-03-23 11:24:37 +0100

    added dynamic content for apps names and descriptions, made responsive for all sizes

--- pages/ecosystem/script.js
+++ pages/ecosystem/script.js
@@ -14,11 +14,42 @@ for (let i=0; i<topNames.length; i++){
 }
 
 const detailedData = [
-  {name:"Chainers.IO", description:"Chainers is the next level Metaverse with infinite P2E possibilities in the unique pixeled 3D packaging"},
-  {name:"Naga Kingdom", description:"The most entertaining .io game on WildCoin"},
-  {name:"Hoglympics", description:"Exhilarating Play-to-Earn (P2E) Olympic Games on Solana - easy to pick up yet hard to master"},
-  {name:"Solview", description:"Wallet checker, NFT Checker and Live streaming charts for Wildcoin tokens"},
-  {name:"Psyker", description:"Welcome to the Pskyerverse. Immerse yourself into a fighting experience like no other, blending science-fiction and post-apocalyptic themes"},
-  {name:"Salad Cat Coin", description:"Meet Salad Cat, the coolest memecoin on Wildcoin"},
+  {name:"Chainers.IO", description:"Chainers is the next level Metaverse with infinite P2E possibilities..."},
+  {name:"Naga Kingdom", description:"The most entertaining .io game on WildCoin is here..."},
+  {name:"Hoglympics", description:"Exhilarating Play-to-Earn (P2E) Olympic Games on Wildcoin..."},
+  {name:"Solview", description:"Wallet checker, NFT Checker and Live streaming charts for Wildcoin tokens."},
+  {name:"Psyker", description:"Welcome to the Pskyerverse. Immerse yourself into a fighting experience like no other..."},
+  {name:"Salad Cat Coin", description:"Meet Salad Cat, the coolest memecoin on Wildcoin."},
 ]
 
+const appListNames = document.getElementsByClassName('app-list-name');
+const appListDesc = document.getElementsByClassName('app-list-desc');
+
+
+for (let i=0; i<appListNames.length; i++){
+  appListNames[i].innerHTML= detailedData[i].name;
+}
+
+for (let i=0; i<appListDesc.length; i++){
+  appListDesc[i].innerHTML= detailedData[i].description;
+}
+
+// Toggle between hiding and showing the dropdown content
+
+function toggleDropdown() {
+  document.getElementById("dropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('#search-button')) {
+    let dropdowns = document.getElementsByClassName("dropdown-content");
+    let i;
+    for (i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
